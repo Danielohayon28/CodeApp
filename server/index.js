09ff97b4377d0b81c1ccf823157fe08d1763e7ce
danielohayon28 <danielohayon420@gmail.com>
2023-08-09 11:01:07 +0300

    Check

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const app = express();
 app.get("/", (request, response) => {
     response.send("Hi there");
 });
-
+app.get("/check/2", (request, response) => {
+    response.send("Hi there");
+});
 app.listen(5001, () => {
     console.log("Listen on the port 5001...");
 });
