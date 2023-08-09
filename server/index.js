a78886439a8ca859220874a55b9ad302cfa12ba6
danielohayon28 <danielohayon420@gmail.com>
2023-08-09 11:11:34 +0300

    Check6

--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
+const http = require("http");
+const mongoose = require("mongoose");
+const socketIO = require("socket.io");
+const codeRoutes = require("./routes/codeRouter");
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/code", codeRoutes);
+const PORT = 8000;
+const MAX_STUDENTS_PER_CLASS = 1;
 
+const server = http.createServer(app); //http server with express app
+
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.get("/check/2", (request, response) => {
     response.send("Hi there");
